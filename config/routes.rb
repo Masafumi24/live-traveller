3ea340ac7daa6a53952fd03027c568afd7da8cfd
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-05-24 17:29:47 +0900

    routing OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "lives#index"
-  resources :lives
-  resources :artists
-  resources :goods
+  resources :lives only: [:index, :new, :create,]
+  resources :artists only: [:new, :create]
+  resources :goods only: [:new, :create]
+  resources :groups only: [:index, :new, :create]
+  resources :messages only: [:index, :create]
 end
