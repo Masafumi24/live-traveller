e21955f20efee2ee5fc0a8795a0cd0dcecaa6c37
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-05-24 17:31:19 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "lives#index"
-  resources :lives only: [:index, :new, :create,]
-  resources :artists only: [:new, :create]
-  resources :goods only: [:new, :create]
-  resources :groups only: [:index, :new, :create]
-  resources :messages only: [:index, :create]
+  resources :lives, only: [:index, :new, :create]
+  resources :artists, only: [:new, :create]
+  resources :goods, only: [:new, :create]
+  resources :groups, only: [:index, :new, :create]
+  resources :messages, only: [:index, :create]
 end
