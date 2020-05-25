eabf9d0c43cc78eca6215d3306462f1265af7aec
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-05-25 13:19:26 +0900

    group controller OK

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,18 @@
 class GroupsController < ApplicationController
+  # before_action :only, [:new]
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+  end
+
+  private
+
+  def group_params
+    params[:group].permit(:name)
+  end
+  
 end
